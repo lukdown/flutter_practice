cbe993bc12e7cc96347caca4925cc300bd80d095
seed-yoo <lukdown10@naver.com>
2024-04-24 11:12:47 +0900

    ..

---
+++ lib/ex01.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Ex01 extends StatelessWidget {
+  const Ex01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      
+      body: Container(),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+      ),
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
